598705c7f65fb7c0b2304bf2725d81d4f0495f05
Titus Fortner <titusfortner@gmail.com>
2016-10-05 11:00:14 -0500

    add conversion methods

--- lib/watir_model.rb
+++ lib/watir_model.rb
@@ -55,6 +55,8 @@ class WatirModel
       value = case
               when object == 'Key'
                 result
+              when object == Symbol
+                result.to_sym
               when object == String
                 result.to_s
               when object == Integer
@@ -62,7 +64,7 @@ class WatirModel
               when object == Float
                 result.to_f
               when object == 'Boolean'
-                eval result
+                result.is_a?(String) ? eval(result) : result
               else
                 object.new result
               end
@@ -90,5 +92,37 @@ class WatirModel
     keys.all? { |k| send(k) == other[k] }
   end
   alias_method :==, :eql?
+
+  def self.convert(hash)
+    id = hash[:id]
+    filtered = hash.delete_if { |k| !keys.include?(k) }
+    new(filtered).tap { |m| m.id = id }
+  end
+
+  def to_hash(opt = nil)
+    opt ||= keys
+    opt.each_with_object({}) do |key, hash|
+      value = send(key)
+      hash[key] = process_value(value)
+    end
+  end
+  alias_method :to_h, :to_hash
+
+  def process_value(value)
+    case value
+    when WatirModel
+      value.to_hash
+    when Hash
+      process_value(value)
+    when Array
+      value.map { |v| process_value(v) }
+    else
+      value
+    end
+  end
+
+  def to_json(opt={})
+    to_hash.to_json(opt)
+  end
 
 end
